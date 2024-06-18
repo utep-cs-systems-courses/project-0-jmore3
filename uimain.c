669a17347882f366132b1fb78c8f3ba503929211
jmore3 <84981020+jmore3@users.noreply.github.com>
2024-06-17 21:34:39 -0600

    Update uimain.c
    changes

--- uimain.c
+++ uimain.c
@@ -7,11 +7,15 @@ int main()
 
   while (1) { // Infinite while loop
 
-    printf("Enter command (s for square, t for triangle, a for arrow, q to quit): ");
-    scanf(" %c", &command);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fflush(stdout);		/* stdout only flushes automatically on \n */
+    int c;
+    while ((c = getchar()) == '\n'); /* ignore newlines */
+    if (c == EOF)		     /* terminate on end-of-file */
+      goto done;
 
-    // Given the user command, selects which method to call
-    switch (command) {
+    // Given the user answer, select which method to call
+    switch (c) {
     case 't':
       puts("You selected triangle:");
       print_triangle(5, 7);
@@ -26,10 +30,12 @@ int main()
 	print_char_11x16(c);
       break;
     case 'q':
-      printf("Bye!");
+      puts("Bye!");
       goto done; 		/* terminate */
+    case '\n':
+      break;
     default:
-      printf("Unrecognized option, please try again!\n");
+      printf("Unrecognized option '%c', please try again!\n", c);
     }
   }
 
